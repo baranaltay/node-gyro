f2c0333213e0922f744bef5e857f1fa6d0b98e32
Baran Altay <altaybaran@gmail.com>
2018-11-12 00:40:17 +0300

    first graph

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -14,41 +14,64 @@ var cube,
 	zdeg,
 	zdegstring;
 
-
-window.onload = function () {
-	initPrototypes();
-	initSocket();
-};
-
+var gyroDataMaxTime = 60 * 1000;
+var gyroDataMilliSecond = 500;
+var gyroMaxDataCount = gyroDataMaxTime / gyroDataMilliSecond;
+var gyroDatas = [];
+var dataProvider = [];
+var gyroLocked = false;
 
 function initSocket () {
-	var socket = io('http://192.168.43.45:3000');
+	var socket = io('http://192.168.0.26:3000');
 	socket.on('connect', function () {
 		console.log('server socket connected!');
 	});
+
+	var getDataEnable = false;
 	socket.on('message', function (data) {
 		// console.log('data', data);
 		xdeg = (data.beta).toFixed(1);
 		ydeg = (data.gamma).toFixed(1);
 		zdeg = (data.alpha).toFixed(1);
 
-		xdegstring = (+xdeg - 180) + 'deg';
-		ydegstring = (+ydeg - 180) + 'deg';
-		zdegstring = ((+zdeg - 180) * - 1) + 'deg';
+		xdeg = xdeg - 180;
+		ydeg = ydeg - 180;
+		zdeg = (zdeg - 180) * -1;
+
+		xdegstring = xdeg + 'deg';
+		ydegstring = ydeg + 'deg';
+		zdegstring = +zdeg + 'deg';
 
 		betaElem.innerHTML = xdegstring;
 		gammaElem.innerHTML = ydegstring;
 		alphaElem.innerHTML = zdegstring;
 
 		cube.prefixedStyle('transform', `rotateY(${ydegstring}) rotateX(${xdegstring}) rotateZ(${zdegstring})`);
-		// o.x, o.y, o.z for accelerometer
-		// o.alpha, o.beta, o.gamma for gyro
-		// transform: rotate3d(0.1, 0, 0, 0.5rad)
 	});
 	socket.on('disconnect', function () {
 		console.log('server socket disconnected!');
 	});
 }
+
+setInterval(function (params) {
+	gyroDatas.push({
+		beta: xdeg,
+		gamma: ydeg,
+		alpha: zdeg,
+		time: new Date()
+	});
+
+	if (gyroDatas.length > gyroMaxDataCount) {
+		gyroDatas.splice(0, 1);
+		dataProvider.shift();
+	}
+	dataProvider.push({
+		"date": new Date(),
+		"beta": xdeg,
+		"gamma": ydeg,
+		"alpha": zdeg
+	});
+}, gyroDataMilliSecond);
 
 function initPrototypes () {
 	Element.prototype.prefixedStyle = function (p, style) {
@@ -72,3 +95,77 @@ function initPrototypes () {
 
 	perspective = 2000;
 }
+
+function initChart () {
+	var chart = AmCharts.makeChart("chartdiv", {
+		"type": "serial",
+		"theme": "light",
+		"zoomOutButton": {
+			"backgroundColor": '#000000',
+			"backgroundAlpha": 0.15
+		},
+		"dataProvider": [],
+		"categoryField": "date",
+		"categoryAxis": {
+			"parseDates": true,
+			"minPeriod": "fff",
+			"dashLength": 3,
+			"gridAlpha": 0.15,
+			"axisColor": "#DADADA"
+		},
+		"graphs": [{
+			"id": "g1",
+			"valueField": "beta",
+			"bullet": "round",
+			"bulletBorderColor": "#cc0000",
+			"bulletBorderThickness": 2,
+			"lineThickness": 2,
+			"lineColor": "#cc0000",
+			"negativeLineColor": "#cc0000",
+			"hideBulletsCount": 50
+		},{
+			"id": "g2",
+			"valueField": "gamma",
+			"bullet": "round",
+			"bulletBorderColor": "#00cc00",
+			"bulletBorderThickness": 2,
+			"lineThickness": 2,
+			"lineColor": "#00cc00",
+			"negativeLineColor": "#00cc00",
+			"hideBulletsCount": 50
+		},{
+			"id": "g3",
+			"valueField": "alpha",
+			"bullet": "round",
+			"bulletBorderColor": "#0000cc",
+			"bulletBorderThickness": 2,
+			"lineThickness": 2,
+			"lineColor": "#0000cc",
+			"negativeLineColor": "#0000cc",
+			"hideBulletsCount": 50
+		}],
+		"chartCursor": {
+			"cursorPosition": "mouse"
+		}
+	});
+
+	chart.addListener("rendered", zoomChart);
+	window.myChart = chart;
+	zoomChart();
+
+	function zoomChart () {
+		chart.zoomToIndexes(chart.dataProvider.length - 40, chart.dataProvider.length - 1);
+	}
+
+	setInterval(() => {
+		myChart.dataProvider = dataProvider;
+		myChart.validateData();
+	}, gyroDataMilliSecond);
+}
+
+
+window.onload = function () {
+	initPrototypes();
+	initSocket();
+	initChart();
+};
